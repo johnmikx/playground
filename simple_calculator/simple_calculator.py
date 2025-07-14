ef6f69785c62ffd29517b5f7bfc0db0138c8993f
John Mike Asuncion <johnmikeasuncion17@gmail.com>
2025-07-14 16:50:32 +0800

    feat: add simple calculator

---
+++ simple_calculator/simple_calculator.py
@@ -0,0 +1,14 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+sum = num1 + num2
+print("Sum:", sum)
+
+difference = num1 - num2
+print("Difference:", difference)
+
+product = num1 * num2
+print("Product:", product)
+
+quotient = num1 / num2
+print("Quotient:", quotient)
