ec8267ac3c40922f8b297a9419410a2ca36e1e93
John Mike Asuncion <johnmikeasuncion17@gmail.com>
2025-07-14 16:31:06 +0800

    feat: add even or odd checker

---
+++ even_or_odd_checker/even_or_odd_checker.py
@@ -0,0 +1,14 @@
+number = int(input("Enter a number: "))
+if number % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
+
+print("This is a basic if-else structure.")
+print("Try different numbers!")
+
+remainder = number % 2
+print("Remainder when divided by 2 is:", remainder)
+print("0 means even, 1 means odd.")
+print("This is a simple even or odd checker.")
+print("You can use this to check any integer.")
