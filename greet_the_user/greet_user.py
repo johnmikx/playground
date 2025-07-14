01752aa31a61dbbecbd8eadd64faae72d6c0455a
John Mike Asuncion <johnmikeasuncion17@gmail.com>
2025-07-14 15:16:04 +0800

    feat: add greet the user

---
+++ greet_the_user/greet_user.py
@@ -0,0 +1,12 @@
+name = input("What's your name? ")
+greeting = "Hello, " + name + "!"
+print(greeting)
+print("Welcome to Python programming!")
+
+age = input("What's your age? ")
+print("Nice, you are", age, "years old!")
+
+hobby = input("What is your hobby? ")
+print(f"Cool! I like {hobby} too!")
+print("Let's learn together.")
+print("Goodbye,", name + "!")
