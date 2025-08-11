958bc84669a6a61f36df04232c02f9e0f2506a1a
John Mike Asuncion <johnmikeasuncion17@gmail.com>
2025-08-11 23:40:38 +0800

    feat: addd hrglass star pattern

---
+++ hourglass_star_pattern/hourglass_star_pattern.py
@@ -0,0 +1,9 @@
+def hourglass_pattern(n):
+
+    for i in range(n, 0, -1):
+        print(" " * (n - i) + "* " * i)
+
+    for i in range(2, n + 1):
+        print(" " * (n - i) + "* " *  i)
+
+hourglass_pattern(5)
