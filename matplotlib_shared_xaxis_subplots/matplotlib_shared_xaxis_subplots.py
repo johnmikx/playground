c8e59fe473e3c7b200ff23654b957f741c9333db
John Mike Asuncion <johnmikeasuncion17@gmail.com>
2025-08-11 23:49:47 +0800

    feat: plt shared xaxis

---
+++ matplotlib_shared_xaxis_subplots/matplotlib_shared_xaxis_subplots.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(0, 2, 100)
+
+fig, (ax, ax2) = plt.subplots(2, 1, sharex=True)
+ax.plot(x, np.exp(x))
+ax2.plot(x, np.exp(x))
+
+ax2.set_ylim(0, 5)
+
+plt.subplots_adjust(hspace=0.3)
+plt.show()
