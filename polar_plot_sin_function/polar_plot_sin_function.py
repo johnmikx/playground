94c1babfc98c8c096edccdfdb268405d06c91143
John Mike Asuncion <johnmikeasuncion17@gmail.com>
2025-08-19 00:14:59 +0800

    chore: bold for title

--- polar_plot_sin_function/polar_plot_sin_function.py
+++ polar_plot_sin_function/polar_plot_sin_function.py
@@ -5,5 +5,5 @@ theta = np.linspace(0, 10 * np.pi, 1000)
 r = np.sin((8 * theta) / 5)
 
 plt.polar(theta, r)
-plt.title('Polar Plot of r = sin(8θ/5)')
+plt.title('Polar Plot of r = sin(8θ/5)', fontweight='bold')
 plt.show()
